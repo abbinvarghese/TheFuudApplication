1f781a640feb2fa20446c7aa3007a7b55648c5e1
AbbinVarghese <paadam2@gmail.com>
2016-06-19 14:11:57 +0530

    Initial Commit

---
+++ TheFuudApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFuudApplication
+//
+//  Created by Abbin Varghese on 19/06/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheFuudApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheFuudApplication
+//
+//  Created by Abbin Varghese on 19/06/16.
+//  Copyright © 2016 Abbin Varghese. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
